61ccb3363fc3583a77e61dc1334a47dee89030b6
canglanQS <jxhaiyangzhijia@126.com>
2015-03-27 00:08:59 +0800

    主界面左隐藏栏界面跳转
    1.左边list点击item内的图片可跳转到新的界面，html及class都已经建好，可以自行填充；

--- app/src/main/java/smallf/CycleNow/MainFrameView.java
+++ app/src/main/java/smallf/CycleNow/MainFrameView.java
@@ -1,11 +1,16 @@
 package smallf.CycleNow;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.view.LayoutInflater;
+import android.widget.Adapter;
+import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.support.v4.widget.DrawerLayout;
@@ -13,8 +18,11 @@ import android.view.KeyEvent;
 import android.widget.Toast;
 import android.widget.SimpleAdapter;
 import android.widget.ListView;
+import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.List;
+import android.content.Context;
 
 
 /**
@@ -24,9 +32,9 @@ public class MainFrameView extends Activity {
 
     private boolean bLeftViewState = false;
 
-    SimpleAdapter listItemAdapter;  // ListView的适配器
-    ArrayList<HashMap<String, Object>> listItem;  // ListView的数据源，这里是一个HashMap的列表
+    DrawerLayout mDrawerLayout;
     ListView myList;  // ListView控件
+    List<Map<String, Object>> m_Data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +42,8 @@ public class MainFrameView extends Activity {
         setContentView(R.layout.mainframe);
         getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,R.layout.mycustomtitle);
 
-        final DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        myList = (ListView) findViewById(R.id.mainframe_left_list);
 
         ((ImageButton) findViewById(R.id.header_left_btn)).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,11 +58,95 @@ public class MainFrameView extends Activity {
             }
         });
 
-/*        listItem = new ArrayList<HashMap<String, Object>>();
-        listItemAdapter = new SimpleAdapter(this, listItem, R.layout.mainframe,
-                new String[]{"image", "title", "text"},
-                new int[]{R.id.ItemImage, R.id.ItemTitle, R.id.ItemText});
-        myList.setAdapter(listItemAdapter);*/
+        MyAdapter adapter = new MyAdapter(this);
+        m_Data = getData();
+        myList.setAdapter(adapter);
     }
 
+    private List<Map<String, Object>> getData() {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("img_btn", R.drawable.bluetooth);
+        map.put("description", "路线库");
+        map.put("view", "smallf.CycleNow.RouteDatabase");
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("img_btn", R.drawable.bluetooth);
+        map.put("description", "设备");
+        map.put("view", "smallf.CycleNow.DeviceView");
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("img_btn", R.drawable.bluetooth);
+        map.put("description", "排行榜");
+        map.put("view", "smallf.CycleNow.RankingList");
+        list.add(map);
+
+        return list;
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            if(mDrawerLayout.isDrawerOpen(Gravity.LEFT)){
+                mDrawerLayout.closeDrawer(Gravity.LEFT);
+                return true;
+            }
+            //Toast toast = Toast.makeText(MainFrameView.this, "back", Toast.LENGTH_SHORT);
+            //toast.show();
+        }
+
+        super.onKeyDown(keyCode, event);
+        return true;
+    }
+
+    public final class ViewHolder{
+        public ImageButton imgbtn;
+        public TextView textview;
+    }
+
+    public class MyAdapter extends BaseAdapter{
+        private LayoutInflater mInflater;
+
+        public MyAdapter(Context context){
+            this.mInflater = LayoutInflater.from(context);
+        }
+
+        //stub func
+        @Override
+        public int getCount() {return m_Data.size(); }
+        public Object getItem(int arg0) {return null;}
+        public long getItemId(int arg0) {return 0;}
+        //stub func over
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+            if (convertView == null) {
+                holder=new ViewHolder();
+                convertView = mInflater.inflate(R.layout.mainframe_left_list, null);
+                holder.imgbtn = (ImageButton)convertView.findViewById(R.id.left_list_img_btn);
+                holder.textview = (TextView)convertView.findViewById(R.id.left_list_textview);
+                convertView.setTag(holder);
+            }else {
+                holder = (ViewHolder)convertView.getTag();
+            }
+
+            holder.textview.setText((String)m_Data.get(position).get("description"));
+            holder.imgbtn.setBackgroundResource((Integer)m_Data.get(position).get("img_btn"));
+            holder.imgbtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent in = new Intent();
+                    in.setClassName( getApplicationContext(), (String)m_Data.get(position).get("view"));
+                    startActivity(in);
+                    mDrawerLayout.closeDrawer(Gravity.LEFT);
+                }
+            });
+           return convertView;
+        }
+    }
 }
